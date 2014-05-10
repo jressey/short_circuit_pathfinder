5ea585e64bca060a17e5cccfa0e381715d383de4
jressey <jressey@gmail.com>
2014-05-10 13:46:28 -0400

    more work

--- short_circuit.rb
+++ short_circuit.rb
@@ -2,7 +2,6 @@ module ShortCircuitPathfinder
 	class ShortCircuit
 		
 		def initialize
-			@point_controller = PointController.new
 			@path_segment_controller = PathSegmentsController.new
 		end
 
@@ -10,31 +9,19 @@ module ShortCircuitPathfinder
 			@path_segments = path_segments
 		end
 
-		def get_all_paths_for_point(point_letter, path_segments)
-			return path_segments.select { |p| p[0] == point_letter }
-		end
-
 		def get_sum_of_distances(path_segments)
 			return path_segments.inject(0) { |sum,p| sum + p[2] }
 		end
 
 		def get_all_possible_paths(path_segments)
-			return self.get_all_paths_for_all_points(path_segments)
-		end
-
-		def get_all_paths_for_all_points(path_segments)
-			array_of_point_paths = Array.new
-			@point_controller.extract_points(path_segments).each do |p|
-				array_of_point_paths.push(p, self.get_all_paths_for_point(p, path_segments))
-			end
-			return array_of_point_paths
+			return @path_segment_controller.get_all_possible_paths(path_segments)
 		end
 
 	end
 
-	class PointController
+	class PointExtractor
 
-		def extract_points(path_segments)
+		def self.extract_points(path_segments)
 			points = Array.new
 			path_segments.each do |p|
 				points.push(p[0], p[1])
@@ -45,14 +32,35 @@ module ShortCircuitPathfinder
 
 	class PathSegmentsController
 		@path_segments = []
-		
-		def set_all_path_segments(path_segments)
-			@path_segments = path_segments
-		end
 
 		def get_all_path_segments
 			@path_segments
 		end
 
+		def get_all_possible_paths(path_segments)
+			set_all_path_segments(@path_segments)
+			array_of_point_paths = Array.new
+			PointExtractor.extract_points(path_segments).each do |p|
+				array_of_point_paths.push(p => get_all_paths_for_point(p, path_segments))
+			end
+			array_of_point_paths
+		end
+
+		def set_all_path_segments(path_segments)
+			@path_segments = path_segments
+		end
+
+		def get_all_paths_for_point(point_letter, path_segments)
+			return path_segments.select { |p| p[0] == point_letter }
+		end
+
+		#need to make an array of all possible paths then add up the numbers to find the lowest sum
+		#then compare that array to the original path segments array and return those paths which are not included
+
+		#make the array by starting at a, getting the first paths [1] value unless that path is already represented
+
+		def get_first_path_for_all_path_segments
+		end
+	
 	end
 end
--- short_circuit_test.rb
+++ short_circuit_test.rb
@@ -39,30 +39,23 @@ class TestShortCircuit < Test::Unit::TestCase
 
 		@all_possible_paths = [[0], [0]] #dummy placeholder result
 
-		@short_circuit = ShortCircuitPathfinder::ShortCircuit.new											
+		@short_circuit = ShortCircuitPathfinder::ShortCircuit.new	
+		@path_segment_controller = ShortCircuitPathfinder::PathSegmentsController.new										
 	end
 
 	def test_short_circuit_pathfinder
 		assert_equal(@path_segments, @short_circuit.find_shortest_path(@input_path_segments))
 	end
 
-	# def test_get_start_point
-	# 	assert_equal('A', ShortCircuitPathfinder::ShortCircuit.get_start_point(@input_path_segments))
+	# def test_find_all_paths_for_point
+	# 	assert_equal(@all_paths_for_a, @short_circuit.get_all_paths_for_point('A', @input_path_segments))
 	# end
-
-	# def test_get_end_point
-	# 	assert_equal('G', ShortCircuitPathfinder::ShortCircuit.get_end_point(@input_path_segments))
-	# end
-
-	def test_find_all_paths_for_point
-		assert_equal(@all_paths_for_a, @short_circuit.get_all_paths_for_point('A', @input_path_segments))
-	end
 
 	def test_get_sum_of_all_paths_for_point
 		assert_equal(50 + 250, @short_circuit.get_sum_of_distances(@path_for_sum_of_distances))
 	end
 
 	def test_get_all_possible_paths
-		assert_equal(@all_possible_paths, @short_circuit.get_all_possible_paths(@input_path_segments))
+		assert_equal(@all_possible_paths, @path_segment_controller.get_all_possible_paths(@input_path_segments))
 	end
 end
